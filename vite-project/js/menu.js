7cc57c23e815c6330e37e570ad04537214e38ab1
lbernice <bernicel677@gmail.com>
2023-11-21 12:32:16 -0500

    k

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,8 +1,72 @@
-const sonny = [
+const lego = [
     {
-        series: "Cat Life",
+        series: "Land Rover Classic Defender",
         image: "",
-        price: "$12.00",
+        price: "$239.00",
+        pieces: "100",
+        ages: "18",
+        type: "Automobile",
         
-    }
+    },
+    {
+        series: "Chevrolet Corvette 1961",
+        image: "",
+        price: "$239.00",
+        ages: "18",
+        type: "Automobile",
+    },
+    {
+        series: "Land Rover Classic Defender",
+        image: "",
+        price: "$239.00",
+        pieces: "100",
+        ages: "18",
+        type: "Automobile",
+        
+    },
+    {
+        series: "Land Rover Classic Defender",
+        image: "",
+        price: "$239.00",
+        pieces: "100",
+        ages: "18",
+        type: "Automobile",
+        
+    },
+    {
+        series: "Land Rover Classic Defender",
+        image: "",
+        price: "$239.00",
+        pieces: "100",
+        ages: "18",
+        type: "Automobile",
+        
+    },
+    {
+        series: "Land Rover Classic Defender",
+        image: "",
+        price: "$239.00",
+        pieces: "100",
+        ages: "18",
+        type: "Automobile",
+        
+    },
+    {
+        series: "Land Rover Classic Defender",
+        image: "",
+        price: "$239.00",
+        pieces: "100",
+        ages: "18",
+        type: "Automobile",
+        
+    },
+    {
+        series: "Land Rover Classic Defender",
+        image: "",
+        price: "$239.00",
+        pieces: "100",
+        ages: "18",
+        type: "Automobile",
+        
+    },
 ]
