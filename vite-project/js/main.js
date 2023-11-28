6984922c30d49f7750e4dce1e6c9e9405345ba85
lbernice <bernicel677@gmail.com>
2023-11-28 13:11:26 -0500

    bye

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -41,14 +41,21 @@ function createCard(arr){
 createCard(fish);
 
 
-DOMSelectors.dark.addEventListener("click", function(){
-    if (document.body.classList.contains("light")) {
-    document.body.classList.add("dark")
-    document.body.classList.remove("light")
-  }
-    else {
+DOMSelectors.light.addEventListener("click", function(){
+    if (document.body.classList.contains("dark")) {
     document.body.classList.add("light")
     document.body.classList.remove("dark")
   }
 });
+    
+DOMSelectors.dark.addEventListener("click", function(){
+  if (document.body.classList.contains("light")) {
+    document.body.classList.add("dark")
+    document.body.classList.remove("light")
+  }
+});
+
+function typeFilter(){
+  
+}
 
